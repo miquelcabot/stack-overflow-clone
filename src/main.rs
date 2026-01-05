482574e9c505cb1a77688d95a711e0afe40bd412
miquelcabot <miquel.cabot@gmail.com>
2026-01-05 15:40:18 +0100

    Initialize logging and database connection in main function

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
-use std::net::SocketAddr;
+#[macro_use]
+extern crate log;
+
+extern crate pretty_env_logger;
 
 use axum::{
     Router,
     routing::{delete, get, post},
 };
+
+use dotenvy::dotenv;
+
+use sqlx::postgres::PgPoolOptions;
 
 mod handlers;
 mod models;
@@ -12,6 +19,23 @@ use handlers::*;
 
 #[tokio::main]
 async fn main() {
+    pretty_env_logger::init();
+    dotenv().ok();
+
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&std::env::var("DATABASE_URL").expect("DATABASE_URL must be set."))
+        .await
+        .expect("Failed to create Postgres connection pool!");
+
+    let recs = sqlx::query!("SELECT * FROM questions")
+        .fetch_all(&pool)
+        .await
+        .unwrap();
+
+    info!("********* Question Records *********");
+    info!("{:?}", recs);
+
     let app = Router::new()
         .route("/question", post(create_question))
         .route("/questions", get(read_questions))
