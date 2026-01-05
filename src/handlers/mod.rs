310a72fab61fa7e99994a5ab1da0ba6f14ba261f
miquelcabot <miquel.cabot@gmail.com>
2026-01-05 15:17:37 +0100

    Add initial project structure with handlers, models, and routing for questions and answers

---
+++ src/handlers/mod.rs
@@ -0,0 +1,46 @@
+use crate::models::*;
+use axum::{Json, response::IntoResponse};
+
+pub async fn create_question(Json(question): Json<Question>) -> impl IntoResponse {
+    Json(QuestionDetail {
+        question_uuid: "question_uuid".to_owned(),
+        title: "title".to_owned(),
+        description: "description".to_owned(),
+        created_at: "created_at".to_owned(),
+    })
+}
+
+pub async fn read_questions() -> impl IntoResponse {
+    Json(vec![QuestionDetail {
+        question_uuid: "question_uuid".to_owned(),
+        title: "title".to_owned(),
+        description: "description".to_owned(),
+        created_at: "created_at".to_owned(),
+    }])
+}
+
+pub async fn delete_question(Json(question_uuid): Json<QuestionId>) {
+    // ...
+}
+
+pub async fn create_answer(Json(answer): Json<Answer>) -> impl IntoResponse {
+    Json(AnswerDetail {
+        answer_uuid: "answer_uuid".to_owned(),
+        question_uuid: "question_uuid".to_owned(),
+        content: "content".to_owned(),
+        created_at: "created_at".to_owned(),
+    })
+}
+
+pub async fn read_answers(Json(question_uuid): Json<QuestionId>) -> impl IntoResponse {
+    Json(vec![AnswerDetail {
+        answer_uuid: "answer_uuid".to_owned(),
+        question_uuid: "question_uuid".to_owned(),
+        content: "content".to_owned(),
+        created_at: "created_at".to_owned(),
+    }])
+}
+
+pub async fn delete_answer(Json(answer_uuid): Json<AnswerId>) {
+    // ...
+}
